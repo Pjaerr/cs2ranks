75936030b20406dbd02ae15df378ea64c8534338
Joshua Jackson <joshcharlesjackson@gmail.com>
2025-07-06 10:23:35 +0100

    gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import svelte from '@astrojs/svelte';
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  site: "https://pjaerr.github.io",
+  base: "cs2ranks",
 });
